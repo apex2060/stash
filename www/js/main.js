57b781ea7b64861939b2f5b4e68dd87c30a3391f
Ryan Quinlan <apex2060@site4u2.com>
2018-03-13 22:15:31 +0000

    test geo 2

--- www/js/main.js
+++ www/js/main.js
@@ -540,16 +540,34 @@ app.controller('MapCtrl', function MapCtrl($scope, $http, config){
 		},
 		map: {
 			init: function(){
-				alert('init')
-				js.map.update();
+				document.addEventListener("deviceready", js.map.getposition, false);
+				// js.map.update();
 			},
-			update: function(){
-				navigator.geolocation.getCurrentPosition(function(pos){
-					js.map.load(pos.coords, $scope.params.id)
-				})
+			getposition: function(){
+				navigator.geolocation.getCurrentPosition(function(position) {
+					alert('Latitude: '          + position.coords.latitude          + '\n' +
+					'Longitude: '         + position.coords.longitude         + '\n' +
+					'Altitude: '          + position.coords.altitude          + '\n' +
+					'Accuracy: '          + position.coords.accuracy          + '\n' +
+					'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+					'Heading: '           + position.coords.heading           + '\n' +
+					'Speed: '             + position.coords.speed             + '\n' +
+					'Timestamp: '         + position.timestamp                + '\n');
+					js.map.load(position.coords, $scope.params.id)
+				}, function(error) {
+					alert('code: '    + error.code    + '\n' +
+					'message: ' + error.message + '\n');
+				});
 			},
+			// update: function(){
+			// 	navigator.geolocation.getCurrentPosition(function(pos){
+			// 		js.map.load(pos.coords, $scope.params.id)
+			// 	})
+			// },
 			load: function(geo, category){
+				alert('load')
 				$http.get('https://dashboard.stashmob.co/cloud/mongo/locations?objectId='+$scope.params.id).then(function(r){
+					alert(JSON.stringify(r.data));
 					$scope.loc = r.data;
 					js.map.display(geo, $scope.loc);
 				})
